dff7a050f277a2c859751c6b418d6668ca532df3
root <yashmathur123123@gmail.com>
2020-04-02 19:00:02 +0530

    corrected minor issues

--- venv/main.py
+++ venv/main.py
@@ -1,7 +1,7 @@
 from kivy.app import App
 from kivy.uix.codeinput import CodeInput
 from kivy.lang import Builder
-#:include Newcan.kv
+
 
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.graphics import *
@@ -14,7 +14,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
 
 
-class Newcan(Widget):
+class searchitemclass(Widget):
     pass
 
 
@@ -26,8 +26,9 @@ class MainApp(App):
         screen1 = Screen(name='mainscreen')
         screen1.add_widget(self.connect_page)
         self.screen_manager.add_widget(screen1)
-        self.info_page = Newcan()
-        screen = Screen(name='Info')
+        ########################### attaching the search item screen #################
+        self.info_page = searchitemclass()
+        screen = Screen(name='searchitemscreen')
         screen.add_widget(self.info_page)
         self.screen_manager.add_widget(screen)
         return self.screen_manager
@@ -35,11 +36,11 @@ class MainApp(App):
     def mainscreen_to_searchitemclass(self):
         print('ghfgjh')
         self.screen_manager.transition.direction = 'left'
-        self.screen_manager.current = 'Info'
+        self.screen_manager.current = 'searchitemscreen'
 
     def searchitemclass_to_mainscreen(self):
         print('ghfgjh')
-        self.screen_manager.transition.direction = 'up'
+        self.screen_manager.transition.direction = 'right'
         self.screen_manager.current = 'mainscreen'
 
 
